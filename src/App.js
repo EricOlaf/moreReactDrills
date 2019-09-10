1a8a181b1f93c61262c3ffb437f00590abef3387
Eric Olaveson <olavesoneric@gmail.com>
2019-09-10 11:07:52 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state={
+    list:["Eric", "Abby", "Jake", "Logan", "Sean"],
+    input:""
+  }
+  inputHandler(e){
+    this.setState({input:e})
+  }
+  render(){
+    let listItems = this.state.list.map((e, i)=>{
+      if(e.toLowerCase().includes(this.state.input)){
+        return <h2 key={i}>{e}</h2>
+      }
+    })
+    return (
+      <div className="App">
+        <input value={this.state.input} placeholder="stuffs" onChange={(e)=>{this.inputHandler(e.target.value)}}/>
+        {listItems}
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
