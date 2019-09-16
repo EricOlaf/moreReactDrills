f33e0278c591b1ef4ad1ee073347582585ec7682
Eric Olaveson <olavesoneric@gmail.com>
2019-09-15 20:54:05 -0500

    start again on 8

--- src/App.js
+++ src/App.js
@@ -31,36 +31,52 @@
  
 // export default App;
 
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-import "./App.css";
+// import "./App.css";
 
-import axios from "axios";
+// import axios from "axios";
 
-class App extends Component {
-  state = {
-    lukeSkywalker: ""
-  };
+// class App extends Component {
+//   state = {
+//     lukeSkywalker: ""
+//   };
   
 
-  componentDidMount() {
-    axios.get('https://swapi.co/api/people/1/').then(response => {
-      this.setState({
-        lukeSkywalker: response.data
-      });
-    });
-  }
+//   componentDidMount() {
+//     axios.get('https://swapi.co/api/people/1/').then(response => {
+//       this.setState({
+//         lukeSkywalker: response.data
+//       });
+//     });
+//   }
 
-  render() {
-    console.log(this.state)
-    return (
-      <div className="App">
-        <h1>Name: {this.state.lukeSkywalker.name}</h1>
-        <h1>Birth Year: {this.state.lukeSkywalker.birth_year}</h1>
-        <h1>Height: {this.state.lukeSkywalker.height}</h1>
-        <h1>Eye Color: {this.state.lukeSkywalker.eye_color}</h1>
-      </div>
-    );
+//   render() {
+//     console.log(this.state)
+//     return (
+//       <div className="App">
+//         <h1>Name: {this.state.lukeSkywalker.name}</h1>
+//         <h1>Birth Year: {this.state.lukeSkywalker.birth_year}</h1>
+//         <h1>Height: {this.state.lukeSkywalker.height}</h1>
+//         <h1>Eye Color: {this.state.lukeSkywalker.eye_color}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, {Component} from 'react'
+import axios from 'axios'
+import './App.css'
+
+class App extends Component{
+  state = {}
+  render(){
+    return(<div>
+      <h1>Luke Skywalker</h1>
+    </div>)
   }
 }
 
