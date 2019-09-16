183e5b4baeb53ccd6e6d869a9b9f6f712443f178
Eric Olaveson <olavesoneric@gmail.com>
2019-09-15 21:03:15 -0500

    done w eight again

--- src/App.js
+++ src/App.js
@@ -2,81 +2,29 @@
 /*NUMBER EIGHT*/
 ///////////////////////////////////////
 
-// import React, { Component } from 'react';
-// import './App.css'
-// import axios from 'axios';
-
-// class App extends Component {
-//   state = { 
-//     characters : {}
-//    }
-
-//    componentDidMount(){
-//      axios.get("https://swapi.co/api/people/1")
-//      .then( res => {
-//        const data = res.data
-//        this.setState({characters: data})
-//      })
-//    }
-
-//   render() { 
-//     console.log(this.state)
-//     return ( 
-//       <div className="App">
-//         <h1>The party is here</h1>
-//       </div>
-//      );
-//   }
-// }
- 
-// export default App;
-
-// import React, { Component } from "react";
-
-// import "./App.css";
-
-// import axios from "axios";
-
-// class App extends Component {
-//   state = {
-//     lukeSkywalker: ""
-//   };
-  
-
-//   componentDidMount() {
-//     axios.get('https://swapi.co/api/people/1/').then(response => {
-//       this.setState({
-//         lukeSkywalker: response.data
-//       });
-//     });
-//   }
-
-//   render() {
-//     console.log(this.state)
-//     return (
-//       <div className="App">
-//         <h1>Name: {this.state.lukeSkywalker.name}</h1>
-//         <h1>Birth Year: {this.state.lukeSkywalker.birth_year}</h1>
-//         <h1>Height: {this.state.lukeSkywalker.height}</h1>
-//         <h1>Eye Color: {this.state.lukeSkywalker.eye_color}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
 import React, {Component} from 'react'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component{
-  state = {}
+  state = {
+    myBoyLuke : {}
+  }
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/1/").then(res=> {
+      this.setState({myBoyLuke: res.data})
+    }
+    )
+  }
   render(){
-    return(<div>
-      <h1>Luke Skywalker</h1>
-    </div>)
+    let {name, height, mass} = this.state.myBoyLuke;
+    return(
+    <div className="App">
+      <h1>{name}</h1>
+      <h3>{height}</h3>
+      <h3>{mass}</h3>
+    </div>
+    )
   }
 }
 
