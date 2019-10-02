611e13d4c658c85703d0962dd1033dd261e7cb2c
Eric Olaveson <olavesoneric@gmail.com>
2019-10-02 11:27:46 -0500

    changes on nine

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@
 /*NUMBER NINE*/
 ///////////////////////////////////////
 
-import React, {Component} from 'react'
-import './App.css'
+// import React, {Component} from 'react'
+// import './App.css'
 
-import {Link} from "react-router-dom";
-import router from "./router";
+// import {Link} from "react-router-dom";
+// import router from "./router";
 
-class App extends Component{
-  state = {
+// class App extends Component{
+//   state = {
     
-  }
+//   }
  
-  render(){
+//   render(){
     
-    return(
-    <div className="App">
-      <nav>
-        <ul>
-          <Link to="/">HOME</Link>
-          <Link to="/signup">Signupupupupup</Link>
-          <Link to="/details">DEEEETS</Link>
-        </ul>
-      </nav>
-      {router}
-    </div>
-    )
-  }
-}
+//     return(
+//     <div className="App">
+//       <nav>
+//         <ul>
+//           <Link to="/">HOME</Link>
+//           <Link to="/signup">Signupupupupup</Link>
+//           <Link to="/details">DEEEETS</Link>
+//         </ul>
+//       </nav>
+//       {router}
+//     </div>
+//     )
+//   }
+// }
 
-export default App;
+// export default App;
